48aa9d9035f1fd0d29bad95ed975419d9e1bdf31
Justin Dong <justindong@codehs.com>
2022-06-16 15:26:49 -0500

    Send bunks only if at least 2 users

--- jitterbunk/management/commands/initbunks.py
+++ jitterbunk/management/commands/initbunks.py
@@ -12,6 +12,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         users = list(User.objects.all())
+        if len(users) < 2:
+            return
         for i in range(20):
             user1, user2 = sample(users, 2)
             bunk = Bunk(from_user=user1, to_user=user2, time=timezone.now())
